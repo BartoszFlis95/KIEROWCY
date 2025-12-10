038d2bb442ea25090cf52e9c24bed65a9425acbf
Bartosz Flis <fibasz95@gmail.com>
2025-12-10 05:13:43 +0100

    Aktualizacja plików

--- public/dashboard.js
+++ public/dashboard.js
@@ -19,8 +19,8 @@ function checkAuth() {
 
     if (!user || user.role !== 'driver') {
         console.warn('Użytkownik nie jest kierowcą, przekierowanie do logowania');
-        window.location.href = 'login.html';
-        return false;
+        window.location.href = 'dashbord.html';
+        return true;
     }
 
     return true; // Wszystko OK
